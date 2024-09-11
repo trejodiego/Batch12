25156f05208f4564735bb00d95b8d55258b84864
trejo <trejo@Pnuts>
2024-09-10 19:10:13 -0700

    practice

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,14 +2,9 @@ package homework;
 
 public class GitHomework {
 
-	public void display() {
-		System.out.println("John Cena");
-	}
-	/**
-			This is coming from github
-   				Java is Fun
-       				Github is EASY
-	   			API is not fun...
 
-	*/
+	public void getHomework() {
+		System.out.println("Practice More");
+	}
+	
 }
