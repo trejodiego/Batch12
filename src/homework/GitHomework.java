ea0e5696e09f1513707789cff14ffa23228b1781
trejo <trejo@Pnuts>
2024-09-10 17:37:01 -0700

    added new method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,4 +5,8 @@ public class GitHomework {
 	public static void main(String args[]) { 
 		System.out.println("BATCH 12: Git Class Lecture"); 
 	}
+	
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
 }
