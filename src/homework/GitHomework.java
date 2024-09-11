567fc770575587695338c307a5591032c024d296
trejo <trejo@Pnuts>
2024-09-10 17:45:35 -0700

    method1

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -9,4 +9,8 @@ public class GitHomework {
 	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
+	
+	public static void add1(int n, int a) {
+		System.out.println(n + a);
+	}
 }
