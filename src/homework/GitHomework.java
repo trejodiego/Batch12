80064d9a94c7276005f309f00d5a700d06ec534c
trejodiego <trejodiegog@gmail.com>
2024-09-10 18:54:14 -0700

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,4 +5,11 @@ public class GitHomework {
 	public void display() {
 		System.out.println("John Cena");
 	}
+	/**
+			This is coming from github
+   				Java is Fun
+       				Github is EASY
+	   			API is not fun...
+
+	*/
 }
