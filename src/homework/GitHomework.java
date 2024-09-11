f2426964e82f18ebbd44f5b3064495013fbd2cfc
trejo <trejo@Pnuts>
2024-09-10 18:15:35 -0700

    removed code

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,18 +2,5 @@ package homework;
 
 public class GitHomework {
 
-	public static void main(String args[]) { 
-		System.out.println("BATCH 12: Git Class Lecture"); 
-		int age = 12;
-		String name = "John";
-		//This is coming from GitHub
-	}
-	
-	public static void add(int a, int b) {
-		System.out.println(a + b);
-	}
-	
-	public static void add1(int n, int a) {
-		System.out.println(n + a);
-	}
+
 }
