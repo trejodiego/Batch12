a0c708efe3e42288d976ccc0cf8b75070313f59f
trejo <trejo@Pnuts>
2024-09-10 17:28:23 -0700

    com

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -3,6 +3,6 @@ package homework;
 public class GitHomework {
 
 	public static void main(String args[]) { 
-		System.out.println("BATCH 12: Git Class Lecture");
+		System.out.println("BATCH 12: Git Class Lecture"); 
 	}
 }
